3b2a579b0cff900ef161de6c8a7a0b97b892760f
samychaconv-svg <samychaconv@gmail.com>
2026-07-01 11:05:25 -0500

    Change BMI function to format output
    Format BMI output to two decimal places.

--- TALLER.py
+++ TALLER.py
@@ -24,7 +24,7 @@ def bmi(weight, height):
     if height < 1.0 or height > 2.5 or weight < 20 or weight > 200:
         return None
 
-    return weight / height ** 2
+    return f"{weight / height ** 2:.2f}"
 
 
 print(bmi(weight = lb_to_kg(132.277), height = ft_and_inch_to_m(5, 3)))
