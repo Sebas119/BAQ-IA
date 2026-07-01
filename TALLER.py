494d11cfc38349cadabef94f0c1bbc1e4c6bef85
samychaconv-svg <samychaconv@gmail.com>
2026-07-01 10:50:55 -0500

    Change function parameters to fixed values
    Updated function parameters to use specific values instead of variables.

--- TALLER.py
+++ TALLER.py
@@ -1,18 +1,20 @@
+#CODIGO INICIAL
 def bmi(weight, height):
     return weight / height ** 2
 
 
 print(bmi(52.5, 1.65))
+#PARTE LIA 
 
-def ft_and_inch_to_m(ft, inch = 0.0):
+def ft_and_inch_to_m(5, 3 = 0.0):
     return ft * 0.3048 + inch * 0.0254
 
 
-def lb_to_kg(lb):
+def lb_to_kg(132.277):
     return lb * 0.4535923
 
 
-def bmi(weight, height):
+def bmi(132.277, 5, 3):
     if height < 1.0 or height > 2.5 or weight < 20 or weight > 200:
         return None
 
