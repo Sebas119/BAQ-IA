18e7eb1be57b303b850a86e54307ed0668b81b8e
samychaconv-svg <samychaconv@gmail.com>
2026-07-01 10:54:42 -0500

    Refactor bmi function and update usage
    Refactored bmi function and updated print statements for correct weight and height calculations.

--- TALLER.py
+++ TALLER.py
@@ -6,22 +6,28 @@ def bmi(weight, height):
 print(bmi(52.5, 1.65))
 #PARTE LIA 
 
-def ft_and_inch_to_m(5, 3 = 0.0):
+def bmi(weight, height):
+    return weight / height ** 2
+
+
+print(bmi(52.5, 1.65))
+
+def ft_and_inch_to_m(ft, inch = 0.0):
     return ft * 0.3048 + inch * 0.0254
 
 
-def lb_to_kg(132.277):
+def lb_to_kg(lb):
     return lb * 0.4535923
 
 
-def bmi(132.277, 5, 3):
+def bmi(weight, height):
     if height < 1.0 or height > 2.5 or weight < 20 or weight > 200:
         return None
 
     return weight / height ** 2
 
 
-print(bmi(weight = lb_to_kg(176), height = ft_and_inch_to_m(5, 7)))
+print(bmi(weight = lb_to_kg(132.277), height = ft_and_inch_to_m(5, 3)))
 
 #PARTE SEBASTIÁN
 
