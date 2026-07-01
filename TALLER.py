d73b1e0de19e4b22613da04c7a68869e4a1ebf98
Sebas119 <sebastiancondor2902@gmail.com>
2026-07-01 11:01:29 -0500

    Update TALLER.py

--- TALLER.py
+++ TALLER.py
@@ -43,10 +43,10 @@ def bmi(weight, height):
     if height < 1.0 or height > 2.5 or weight < 20 or weight > 200:
         return None
 
-    return weight / height ** 2
+    return f"{weight / height ** 2:.2f}"
 
 
-print(bmi(weight = lb_to_kg(149.914), height = ft_and_inch_to_m(5.643045)))
+print(bmi(weight = lb_to_kg(149.914), height = ft_and_inch_to_m(5.64)))
 
 
 
